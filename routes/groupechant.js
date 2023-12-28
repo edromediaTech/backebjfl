d20caf90186d2b73169263e2a1cc660c51323522
sironel <edromediatech@gmail.com>
2023-12-28 11:32:11 -0500

    get all group route

--- routes/groupechant.js
+++ routes/groupechant.js
@@ -7,7 +7,7 @@ const groupechantCtrl = require('../controllers/groupechant');
 router.post('/',   groupechantCtrl.createGroupechant);
 //router.patch('/:id',  groupechantCtrl.updategroupechant);
 router.delete('/:id', groupechantCtrl.deleteGroupechantById);
-//router.get('/all/', groupechantCtrl.getAllGroupechant);
+router.get('/all/', groupechantCtrl.getAllGroupechants);
 
 
 module.exports = router;
