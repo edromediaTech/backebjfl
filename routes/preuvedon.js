3950eb4ef1cd2caa9d06c12c36681cb58b6bebc8
sironel <edromediatech@gmail.com>
2024-02-13 15:31:32 -0500

    route to display don image

--- routes/preuvedon.js
+++ routes/preuvedon.js
@@ -21,6 +21,20 @@ router.post('/', upload.single('image'), async (req, res) => {
   }
 });
 
+router.get('/preuve/:id', async (req, res) => {
+  try {
+      const image = await Preuvedon.findById(req.params.id);
+      if (!image) {
+          return res.status(404).json({ message: "Image not found" });
+      }
+      res.set('Content-Type', image.contentType);
+      res.send(image.data);
+  } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Server error" });
+  }
+});
+
 // router.post('/preuvedons', preuvedonController.createPreuvedon);
 // router.get('/preuvedons', preuvedonController.getAllPreuvedons);
 // router.get('/preuvedons/:id', preuvedonController.getPreuvedonById);
